c144f6633b6b6e52689e9c6281e479919cf85900
Dominik Sikora <sharkbay@email.cz>
2024-05-29 16:03:02 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import "./style.css";
@@ -14,25 +14,19 @@ import Footer from "./components/Footer";
 import ErrorPage from "./pages/ErrorPage.js";
 
 function App() {
-  const [language, setLanguage] = useState(false);
   return (
     <div className="App">
       <Router>
-        <Nav setLanguage={setLanguage} language={language} />
+        <Nav />
         <Routes>
           <Route exact path="/" element={<Homepage />} />
           {/* <Route exact path="/about" element={<About />} /> */}
           <Route exact path="/contact" element={<Contact />} />
           <Route exact path="/gallery" element={<Gallery />} />
           {/* <Route exact path="/products" element={<Products />} /> */}
-          <Route
-            path="*"
-            element={<ErrorPage />}
-            setLanguage={setLanguage}
-            language={language}
-          />
+          <Route path="*" element={<ErrorPage />} />
         </Routes>
-        <Footer language={language} />
+        <Footer />
       </Router>
     </div>
   );
