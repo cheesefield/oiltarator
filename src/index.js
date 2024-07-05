3968235d81572f3d8de2bae4c061a22b158c3168
Dominik Sikora <sharkbay@email.cz>
2024-07-05 11:51:29 +0200

    formspree and flealet fix test 1

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from "react";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { hydrateRoot } from "react-dom/client";
 import { createRoot } from "react-dom/client";
 
 const container = document.getElementById("root");
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
 if (container.hasChildNodes()) {
-  root.hydrate(<App />);
+  hydrateRoot(document.getElementById("root"), <App />);
 } else {
   root.render(<App />);
 }
