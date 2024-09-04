f1afad59da1789aa74d663cd8828ce2c87310240
Dominik Sikora <sharkbay@email.cz>
2024-09-04 20:30:33 +0200

    new location open, remove react snap

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ function Footer() {
 
   return (
     <div className="footer">
-      <p suppressHydrationWarning={true}>
+      <p>
         {t("footer")} &copy; {currentYear}
       </p>
     </div>
