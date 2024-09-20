f9db8c732d5d71532be11da0d7523a5182eeb7bf
Dominik Sikora <sharkbay@email.cz>
2024-09-20 04:56:23 +0200

    update apple and android icons + manifest routes

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -6,32 +6,32 @@ export default function manifest(): MetadataRoute.Manifest {
     short_name: "OilTarator",
     icons: [
       {
-        src: `${process.env.NEXT_PUBLIC_BASE_URL}/favicon/favicon.ico`,
+        src: `${process.env.NEXT_PUBLIC_BASE_URL}/favicon.ico`,
         sizes: "16x16",
         type: "image/png",
       },
       {
-        src: `${process.env.NEXT_PUBLIC_BASE_URL}/favicon/favicon-16x16.png`,
+        src: `${process.env.NEXT_PUBLIC_BASE_URL}/favicon-16x16.png`,
         sizes: "16x16",
         type: "image/png",
       },
       {
-        src: `${process.env.NEXT_PUBLIC_BASE_URL}/favicon/favicon-32x32.png`,
+        src: `${process.env.NEXT_PUBLIC_BASE_URL}/favicon-32x32.png`,
         sizes: "32x32",
         type: "image/png",
       },
       {
-        src: `${process.env.NEXT_PUBLIC_BASE_URL}/favicon/apple-touch-icon.png`,
+        src: `${process.env.NEXT_PUBLIC_BASE_URL}/apple-touch-icon.png`,
         sizes: "180x180",
         type: "image/png",
       },
       {
-        src: `${process.env.NEXT_PUBLIC_BASE_URL}/favicon/android-chrome-192x192.png`,
+        src: `${process.env.NEXT_PUBLIC_BASE_URL}/android-chrome-192x192.png`,
         sizes: "192x192",
         type: "image/png",
       },
       {
-        src: `${process.env.NEXT_PUBLIC_BASE_URL}/favicon/android-chrome-512x512.png`,
+        src: `${process.env.NEXT_PUBLIC_BASE_URL}/android-chrome-512x512.png`,
         sizes: "512x512",
         type: "image/png",
       },
